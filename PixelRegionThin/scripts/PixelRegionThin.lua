3e3a330e1a3de6df9a998e57c3e041d5f26d48c4
GitLab Runner <gitlab-runner@ip-172-31-13-207.eu-central-1.compute.internal>
2023-04-25 18:00:04 +0000

    Update

--- PixelRegionThin/scripts/PixelRegionThin.lua
+++ PixelRegionThin/scripts/PixelRegionThin.lua
@@ -1,31 +1,43 @@
 --Start of Global Scope---------------------------------------------------------
 
 -- Viewer for showing the results.
-local v = View.create("viewer2D1")
+local v = View.create()
+
+local DELAY = 1000 -- ms between each type for demonstration purpose
 
 --End of Global Scope-----------------------------------------------------------
 
 --Start of Function and Event Scope---------------------------------------------
 
--- Draws two pixel regions on top of an image, before and after thinning.
+--- Draws two pixel regions on top of an image, before and after thinning.
+---@param image Image
+---@param thinRegion Image.PixelRegion
+---@param thrRegion Image.PixelRegion
 local function drawRegionOnImage(image, thinRegion, thrRegion)
   -- Thinned region draw color
-  local dec = View.PixelRegionDecoration.create()
-  dec:setColor(255, 0, 0, 255)
+  local dec = View.PixelRegionDecoration.create():setColor(255, 0, 0, 255)
 
   -- Thresholded region draw color
-  local decThr = View.PixelRegionDecoration.create()
-  decThr:setColor(0, 0, 255, 127)
+  local decThr = View.PixelRegionDecoration.create():setColor(0, 0, 255, 127)
 
   -- Draw image and region
   v:clear()
-  local vId = v:addImage(image)
-  v:addPixelRegion(thrRegion, decThr, nil, vId)
-  v:addPixelRegion(thinRegion, dec, nil, vId)
+  v:addImage(image)
+  v:present()
+  Script.sleep(DELAY)
+
+  v:addPixelRegion(thrRegion, decThr)
+  v:present()
+  Script.sleep(DELAY)
+  
+  v:addPixelRegion(thinRegion, dec)
   v:present()
 end
 
--- Finds lines in an image and returns those as a thin pixel region.
+--- Finds lines in an image and returns those as a thin pixel region.
+---@param image Image
+---@return Image.PixelRegion
+---@return Image.PixelRegion
 local function findThinLines(image)
   -- Generate a smoothed version of the image to account for uneven lighting.
   local smoothImage = image:gauss(100)
@@ -51,6 +63,8 @@ local function main()
 
   -- Show image with detected thin lines.
   drawRegionOnImage(image, thinRegion, thrRegion)
+
+  print('App finished.')
 end
 
 --The following registration is part of the global scope which runs once after startup
